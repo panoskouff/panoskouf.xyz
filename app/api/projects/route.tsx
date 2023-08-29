5e25bdee9db1b0d2670e7ed3123eb3572a931aae
panoskouff <me@panoskouf.xyz>
2023-08-29 09:18:07 +0200

    add all projects route

---
+++ app/api/projects/route.tsx
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import { projects } from '../data';
+
+/* temporary route until we move to a headless cms*/
+export async function GET(
+) {
+  return NextResponse.json({ data: projects });
+}
