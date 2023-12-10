6db3de6c6590b1b4cb3855f5672217b8ed8f9de9
panoskouff <me@panoskouf.xyz>
2023-12-10 05:48:18 +0200

    fix test dom

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,29 +1,27 @@
-// jest.config.mjs
-import nextJest from 'next/jest.js'
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // path to Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
   testEnvironment: 'jest-environment-jsdom',
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['./src/tests/jestPostSetup.ts'],
   moduleNameMapper: {
-    '^#/(.*)$': '<rootDir>/$1',
+    '^#/(.*)$': '<rootDir>/src/$1',
   },
-}
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
 
 /*
-  To test environment variables directly, load them manually in a separate setup
-  script or in your jest.config.js file. For more information,
-  please see:
-  https://nextjs.org/docs/basic-features/environment-variables#test-environment-variables
-   */
+To test environment variables directly, load them manually in a separate setup
+script or in your jest.config.js file. For more information,
+please see:
+https://nextjs.org/docs/basic-features/environment-variables#test-environment-variables
+*/
--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
 
 - Background component should be renamed and merged with Container and also have box shadow and border options
 - clean up not needed css rules and files, for example the theme file
-- fix test dom
+- group all global css files to 1 import
 - add shadow to project image
 - make all pages responsive
 - add responsive header
